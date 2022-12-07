7b1e585b53a34811bf28054333022c037ad10faf
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-07 17:30:18 +0100

    finition de la partie js

--- javascript.js
+++ javascript.js
@@ -10,15 +10,15 @@ function autochoice(){
 };
 function check(playerSelection, computerSelection){
     if (playerSelection===computerSelection){
-        return "égalité"
+        return 0
     }else if (playerSelection=="papier" && computerSelection=="pierre"){
-        return "tu a gagner"
+        return 1
     }else if (playerSelection=="pierre" && computerSelection=="ciseaux"){
-        return "tu a gagner"
+        return 1
     }else if (playerSelection=="ciseaux" && computerSelection=="papier"){
-        return "tu a gagner"
+        return 1
     }else {
-        return "tu a perdu"
+        return -1
     }
 };
 
@@ -26,4 +26,27 @@ function playerchoice(){
     let choix = prompt("entrer pierre/papier/ciseaux");
     return choix.toLocaleLowerCase()
 }
-console.log(playerchoice())
+function game(){
+    let tour = 0;
+    let ordi = 0;
+    let joueur = 0;
+    let action;
+    let score;
+    while(tour<5){
+        action = check(playerchoice(),autochoice())
+        if (action==-1){
+            alert("Vous avez perdu")
+            ordi++;
+            tour++;
+        }else if (action==0){
+            alert("égalité, on la refait")
+        }
+        else{
+            alert("Vous avez gagner")
+            joueur++;
+            tour++;
+        }
+        alert(`vous ${joueur} : ${ordi} ordinateur`)
+    };
+}
+game()
