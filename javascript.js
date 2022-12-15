e347d4d5ac5149a278f0e9f58f04fe2538e4b395
Fantoooo <celian.lecorvoisier@orange.fr>
2022-12-15 19:07:20 +0100

    fonctionne maintenant avec des boutons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/*choix automatique et aléatoire de l'ordinateur */
 function autochoice(){
     const Number = Math.floor(Math.random()*3)
     if (Number===1){
@@ -8,45 +9,54 @@ function autochoice(){
         return "ciseaux"
     };
 };
+
+/*vérifie les résultats, et change  affiche ce qui c'est passer  */
 function check(playerSelection, computerSelection){
+    const child = document.querySelector('#resultat')
     if (playerSelection===computerSelection){
+        child.textContent = "égalité"
         return 0
     }else if (playerSelection=="papier" && computerSelection=="pierre"){
+        child.textContent = "victoire"
         return 1
     }else if (playerSelection=="pierre" && computerSelection=="ciseaux"){
+        child.textContent = "victoire"
         return 1
     }else if (playerSelection=="ciseaux" && computerSelection=="papier"){
+        child.textContent = "victoire"
         return 1
     }else {
+        child.textContent = "défaite"
         return -1
     }
 };
+let scoredata = {ordinateur:0,utilisateur:0}
 
-function playerchoice(){
-    let choix = prompt("entrer pierre/papier/ciseaux");
-    return choix.toLocaleLowerCase()
-}
-function game(){
-    let tour = 0;
-    let ordi = 0;
-    let joueur = 0;
-    let action;
-    let score;
-    while(tour<5){
-        action = check(playerchoice(),autochoice())
-        if (action==-1){
-            alert("Vous avez perdu")
-            ordi++;
-            tour++;
-        }else if (action==0){
-            alert("égalité, on la refait")
-        }
-        else{
-            alert("Vous avez gagner")
-            joueur++;
-            tour++;
-        }
-        alert(`vous ${joueur} : ${ordi} ordinateur`)
-    };
-}
-game()
+/* actualisation des scores */
+function score(winorloose){
+    const score = document.querySelector('#score')
+    if (winorloose==0){
+        return 
+    }else if (winorloose==1){
+        scoredata.utilisateur=1 + scoredata.utilisateur
+        score.textContent=`Ordi ${scoredata.ordinateur} : ${scoredata.utilisateur} vous`
+    }else{
+        scoredata.ordinateur=1+ scoredata.ordinateur
+        score.textContent=`Ordi ${scoredata.ordinateur} : ${scoredata.utilisateur} vous`
+    }
+    if (scoredata.ordinateur == 5){document.location.href="./perdu.html"}
+    if (scoredata.utilisateur == 5){document.location.href="./victoire.html"}
+};
+/*verifie si bouton cliquer */
+const pierre = document.querySelector("#pierre")
+const papier = document.querySelector("#papier")
+const ciseaux = document.querySelector("#ciseaux")
+pierre.addEventListener('click', ()=>{
+    score(check(pierre.id,autochoice()))
+})
+papier.addEventListener('click', () =>{
+    score(check(papier.id,autochoice()))
+});
+ciseaux.addEventListener('click', () =>{
+    score(check(ciseaux.id,autochoice()))
+});
